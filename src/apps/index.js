73e07511cb35454b4978355fce5a153bcceccc0a
sirojkhuja <sirojxuja@gmail.com>
2022-10-10 15:32:36 +0500

    Testing bot functions

--- src/apps/index.js
+++ src/apps/index.js
@@ -9,22 +9,7 @@ const App = () => {
     const { user } = useAPI()
 
     useEffect(() => {
-        let webApp = window?.Telegram?.WebApp
-        webApp.expand()
-        let mainButton = webApp.MainButton
-        mainButton.show()
-        mainButton.setParams({
-            'text': `Notifications`,
-            'color': "#2ECC71",
-            'text_color': "#F7F9F9"
-        })
-        mainButton.onClick(() => {
-            mainButton.setParams({
-                'text': `Notifications changed`,
-                'color': "#2ECC71",
-                'text_color': "#F7F9F9"
-            })  
-        })
+        window?.Telegram?.WebApp.expand()
     });
     let currentUser = user ? user.user : JSON.parse(localStorage.getItem('user'))
     let token = user ? user.token : localStorage.getItem('token')
