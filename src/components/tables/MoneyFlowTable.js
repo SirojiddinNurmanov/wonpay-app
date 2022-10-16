bb79e27ad138b3591cba223bcfa3c939a930ecde
sirojkhuja <sirojxuja@gmail.com>
2022-10-16 15:18:48 +0500

    Money Flow Page + Table

---
+++ src/components/tables/MoneyFlowTable.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react"
+import Table from "react-bootstrap/Table"
+
+const MoneyFlowTable = () => {
+    return (
+        <Table striped bordered hover size="sm" responsive className="process-table">
+            <thead>
+                <tr>
+                    <th>Ism</th>
+                    <th>Summa</th>
+                    <th>Foyda</th>
+                    <th>Jami Foyda</th>
+                    <th>Harajat</th>
+                    <th>Mavjud Mablag'</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦20.000.000</td>
+                    <td>+$10</td>
+                    <td>$73.250</td>
+                    <td>-$5.000</td>
+                    <td>$65.240</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
+}
+
+export default MoneyFlowTable
--- src/pages/admin/MoneyFlowPage.js
+++ src/pages/admin/MoneyFlowPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import MoneyFlowTable from "../../components/tables/MoneyFlowTable"
 
 import { common } from '../../constants/bottomButtons'
 
@@ -11,121 +12,8 @@ const MoneyFlowPage = () => {
         }
     ]
     return (
-        <Layout buttons={common} title={{ text: "Mavjud mablag':", amount: "₩32.000.000" }}>
-            <div className="request-body-title">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-2">Ism</div>
-                        <div className="col-2">Summa</div>
-                        <div className="col-2">Foyda</div>
-                        <div className="col-2">Jami Foyda</div>
-                        <div className="col-2">Harajat</div>
-                        <div className="col-2">Mavjud Mablag'</div>
-                    </div>
-                </div>
-            </div>
-            <div className="request-body-item">
-                <div className="request-time">22.08.2022</div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="request-body-item">
-                <div className="request-time">05.08.2022</div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <Layout buttons={common} title={{ text: "Mavjud mablag':", amount: "$73.256 12.000.000 so'm", small: true }}>
+            <MoneyFlowTable />
         </Layout>
     )
 }
