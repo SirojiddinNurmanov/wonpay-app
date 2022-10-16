3ab074d29d9049e748357d3bf015f862c9c5e28a
sirojkhuja <sirojxuja@gmail.com>
2022-10-16 15:27:24 +0500

    Profit Page + Table

---
+++ src/components/tables/ProfitTable.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react"
+import Table from "react-bootstrap/Table"
+
+const ProfitTable = () => {
+    return (
+        <Table striped bordered hover size="sm" responsive className="process-table">
+            <thead>
+                <tr>
+                    <th>Ism</th>
+                    <th>Summa</th>
+                    <th>Info</th>
+                    <th>Chek</th>
+                    <th>Foyda</th>
+                    <th>Jami</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>$73.256</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
+}
+
+export default ProfitTable
--- src/pages/admin/ProfitPage.js
+++ src/pages/admin/ProfitPage.js
@@ -1,4 +1,5 @@
 import React from "react"
+import ProfitTable from "../../components/tables/ProfitTable"
 
 import { common } from '../../constants/bottomButtons'
 
@@ -12,121 +13,8 @@ const ProfitPage = () => {
     ]
 
     return (
-        <Layout buttons={common} title={{ text: "Umumiy Foyda:", amount: "$32.000" }}>
-            <div className="request-body-title">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-2">Ism</div>
-                        <div className="col-2">Summa</div>
-                        <div className="col-2">Info</div>
-                        <div className="col-2">Proof</div>
-                        <div className="col-2">Foyda</div>
-                        <div className="col-2">Umumiy</div>
-                    </div>
-                </div>
-            </div>
-            <div className="request-body-item">
-                <div className="request-time">22.08.2022</div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="request-body-item">
-                <div className="request-time">05.08.2022</div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-table">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Farruh Soipov</div>
-                            <div className="col-2">2.000</div>
-                            <div className="col-2">+$10</div>
-                            <div className="col-2">$73.50</div>
-                            <div className="col-2">-$500</div>
-                            <div className="col-2">$65240</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <Layout buttons={common} title={{ text: "Umumiy Foyda:", amount: "$73.256" }}>
+           <ProfitTable />
         </Layout>
     )
 }
