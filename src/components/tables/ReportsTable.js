b31e3db72c3b810bbfc9093a50ff8d6caf08123d
sirojkhuja <sirojxuja@gmail.com>
2022-10-16 15:22:50 +0500

    Reports Page + Table

---
+++ src/components/tables/ReportsTable.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react"
+import Table from "react-bootstrap/Table"
+
+const ReportsTable = () => {
+    return (
+        <Table striped bordered hover size="sm" responsive className="process-table">
+            <thead>
+                <tr>
+                    <th>Ism</th>
+                    <th>Summa</th>
+                    <th>Info</th>
+                    <th>Chek</th>
+                    <th>Foyda</th>
+                    <th>Holati</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Kor>>Uzb"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+                <tr>
+                    <td>Palonchi Pistonchiyev</td>
+                    <td>￦2.000.000</td>
+                    <td>{"Uzb>>Kor"}</td>
+                    <td>Ko'rish</td>
+                    <td>+$10</td>
+                    <td>Yopilgan</td>
+                </tr>
+            </tbody>
+        </Table>
+    )
+}
+
+export default ReportsTable
--- src/pages/admin/ReportsPage.js
+++ src/pages/admin/ReportsPage.js
@@ -4,13 +4,15 @@ import { common } from '../../constants/bottomButtons'
 
 import Layout from "../../layout"
 
+import ReportsTable from "../../components/tables/ReportsTable"
+
 const ReportsPage = () => {
 
     common.middleButtons = false
 
     return (
         <Layout buttons={common} title={{ text: "O'tkazmalar Tarixi:" }}>
-            
+            <ReportsTable />
         </Layout>
     )
 }
