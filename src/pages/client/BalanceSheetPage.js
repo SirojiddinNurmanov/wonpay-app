1f7adb1eec4384c6d109c53a34d2f3749e3185d8
sirojkhuja <sirojxuja@gmail.com>
2022-10-24 20:53:54 +0500

    Fixed BalanceSheet Page

--- src/pages/client/BalanceSheetPage.js
+++ src/pages/client/BalanceSheetPage.js
@@ -4,63 +4,16 @@ import { common } from "../../constants/bottomButtons"
 
 import Layout from "../../layout"
 
+import BalanceSheetTable from "../../components/tables/BalanceSheetTable"
+
 const BalanceSheetPage = () => {
 
     common.middleButtons = false
 
     return (
         <Layout buttons={common} title={{ text: "Oldi Berdilar:" }}>
-            <div className="request-body">
-                <div className="request-body-title">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">Sana</div>
-                            <div className="col-2">Berilgan pul</div>
-                            <div className="col-2">Kurs</div>
-                            <div className="col-2">Qarz</div>
-                            <div className="col-2">Olingan pul</div>
-                            <div className="col-2">Qoldiq</div>
-                        </div>
-                    </div>
-                </div>
-                <div className="request-body-item">
-                    <div className="request-table">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-2">22.09</div>
-                                <div className="col-2">2.000</div>
-                                <div className="col-2">11200</div>
-                                <div className="col-2">200</div>
-                                <div className="col-2">300</div>
-                                <div className="col-2">500</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="request-table">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-2">22.09</div>
-                                <div className="col-2">2.000</div>
-                                <div className="col-2">11200</div>
-                                <div className="col-2">200</div>
-                                <div className="col-2">300</div>
-                                <div className="col-2">500</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="request-table">
-                        <div className="container">
-                            <div className="row">
-                                <div className="col-2">22.09</div>
-                                <div className="col-2">2.000</div>
-                                <div className="col-2">11200</div>
-                                <div className="col-2">200</div>
-                                <div className="col-2">300</div>
-                                <div className="col-2">500</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div className="processes-block">
+                <BalanceSheetTable />
             </div>
         </Layout>
     )
