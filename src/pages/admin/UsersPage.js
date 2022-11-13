6beba1e8015a21c2b64cac48c9ab6439a5575dc1
sirojkhuja <sirojxuja@gmail.com>
2022-11-13 13:44:25 +0500

    Added users page

---
+++ src/pages/admin/UsersPage.js
@@ -0,0 +1,48 @@
+import React, { memo, useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import Spinner from "react-bootstrap/Spinner"
+
+import { getAllUsers } from "../../store/actions"
+import { common } from '../../constants/bottomButtons'
+
+import Layout from '../../layout'
+
+import CarrierCard from "../../components/cards/CarrierCard"
+import UsersModal from "../../components/modals/admin/UsersModal"
+import NoData from "../../components/common/NoData"
+
+const UsersPage = () => {
+    const [usersModal, showUsersModal] = useState(false)
+    const [loader, showLoader] = useState(true)
+    const { allUsers } = useSelector(state => state.app)
+    const dispatch = useDispatch()
+
+    common.middleButtons = false
+
+    useEffect(() => {
+        dispatch(getAllUsers())
+        showLoader(false)
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <Layout buttons={common} title={{ text: "Kuryerlar" }}>
+            <UsersModal
+                show={usersModal}
+                onHide={() => showUsersModal(false)}
+            />
+            {loader && (
+                <div className="center">
+                    <Spinner animation="border" role="status" size="lg">
+                        <span className="visually-hidden">Loading...</span>
+                    </Spinner>
+                </div>
+            )}
+            {allUsers ? allUsers.filter(user => user.role === 'client').map((user, index) => (
+                <CarrierCard key={index} {...user} />
+            )) : (<NoData />)}
+        </Layout>
+    )
+}
+
+export default memo(UsersPage)
