66b71c9ef6dc128b78e35a1d2f0c4446eb0e2634
Sirojkhuja Muminkhodjaev <sirojxuja@gmail.com>
2023-09-09 05:46:42 +0500

    Changed minus/plus signs to red/green colors

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,10 +1,14 @@
 import React, { memo } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { formatAmount } from "../../helpers";
 
 const Header = ({ toAmount = false, balance = false, fromAmount = false }) => {
     const { avatar, role } = useSelector(state => state.app.user.user);
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    console.log(toAmount, balance, fromAmount);
+
     return (
         <div className="header">
             <div className="logo">
@@ -13,18 +17,18 @@ const Header = ({ toAmount = false, balance = false, fromAmount = false }) => {
             <div className="home-profile">
                 {toAmount && (
                     <>
-                        <span className="balance-item">{toAmount}</span>
+                        <span className={"balance-item" + (toAmount > 0 ? " green" : " red")}>{toAmount}</span>
                         <div className="line">|</div>
                     </>
                 )}
                 {balance && (
                     <>
-                        <span className="balance-item">{balance}</span>
+                        <span className={"balance-item" + (balance > 0 ? " green" : " red")}>{(balance ? '$' : '') + formatAmount(balance, true, true)}</span>
                         {/* <div className="line">|</div> */}
                     </>
                 )}
                 {fromAmount && (
-                    <span className="balance-item">{fromAmount}</span>
+                    <span className={"balance-item" + (fromAmount > 0 ? " green" : " red")}>{fromAmount}</span>
                 )}
                 <img className="avatar" src={avatar ?? "/assets/img/icons/profile.png"} alt="Avatar" onClick={() => role === 'client' ? navigate('/profile') : ""}/>
             </div>
